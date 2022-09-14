5d5079d2fb2788c0ab97b039a940e5b1a276b57d
ivantlaseca <ivantlaseca@hotmail.com>
2022-09-14 19:44:07 -0400

    Buttons added

--- src/Progress.js
+++ src/Progress.js
@@ -1,10 +1,15 @@
 import { useContext } from 'react';
 import PageContext from './PageContext';
+import BackButton from './BackButton';
 
 function Progress(){
+    const settingsInfo = useContext(PageContext);
     return(
         <div style={{textAlign: 'left'}}>
             <label>Progress</label>
+            <div style={{textAlign:'center', marginTop:'20px'}}>
+                <BackButton onClick={() => settingsInfo.setShowProgress(false)} />
+            </div>
         </div>
     );
 }
